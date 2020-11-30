b7aca69d06dd2d97ea140f9f83274c0eab353606
bahmbies <y.banana12@gmail.com>
2020-11-30 14:27:38 -0600

    Bones-for-ViewShows

--- src/components/ShowComponents/ViewShows/ViewShows.js
+++ src/components/ShowComponents/ViewShows/ViewShows.js
@@ -1,13 +1,43 @@
-// import { Component } from 'react'
-// import { withRouter } from 'react-router-dom'
-//
-// import { signOut } from '../../api/auth'
-// import messages from '../AutoDismissAlert/messages'
-//
+import React, { useState } from 'react'
+import { Redirect, Link } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from '../../../apiConfig'
+
+const viewShows = props => {
+    const [show, setShow] = useState({ title: '' })
+}
+    
+   const handleSubmit = (event) => {
+      event.preventDefault()
+      axios({
+        url: `${apiUrl}/shows`,
+        method: 'GET',
+        data: { show }
+      })
+      
+        .then(res => this.setState({ showArray: res.data.shows })
+        .catch(console.error)
+}
+
+
+      return (
+          <div>
+            {this.state.showArray.map(show => (
+                <Fragment>
+                    <h2>{show.title}</h2>
+                    <Link to={`/view-shows/${show._id}`}>Link</Link>
+                </Fragment>
+            ))}
+              
+          </div>
+
+      )
+
+
 // class SignOut extends Component {
 //   componentDidMount () {
 //     const { msgAlert, history, clearUser, user } = this.props
-//
+
 //     signOut(user)
 //       .finally(() => msgAlert({
 //         heading: 'Signed Out Successfully',
@@ -17,10 +47,10 @@
 //       .finally(() => history.push('/'))
 //       .finally(() => clearUser())
 //   }
-//
+
 //   render () {
 //     return ''
 //   }
 // }
-//
+
 // export default withRouter(SignOut)
