f0f74e5320e1255b611798a6482ed9eb0d3b1f4c
bahmbies <y.banana12@gmail.com>
2020-12-01 09:13:10 -0600

    Update-Review-Done-Based-On-Update-Show

--- src/components/ReviewComponents/UpdateReview/UpdateReview.js
+++ src/components/ReviewComponents/UpdateReview/UpdateReview.js
@@ -1,26 +1,73 @@
-// import { Component } from 'react'
-// import { withRouter } from 'react-router-dom'
-//
-// import { signOut } from '../../api/auth'
-// import messages from '../AutoDismissAlert/messages'
-//
-// class SignOut extends Component {
-//   componentDidMount () {
-//     const { msgAlert, history, clearUser, user } = this.props
-//
-//     signOut(user)
-//       .finally(() => msgAlert({
-//         heading: 'Signed Out Successfully',
-//         messagE: messages.signOutSuccess,
-//         variant: 'success'
-//       }))
-//       .finally(() => history.push('/'))
-//       .finally(() => clearUser())
-//   }
-//
-//   render () {
-//     return ''
-//   }
-// }
-//
-// export default withRouter(SignOut)
+import React, { useState, useEffect } from 'react'
+import { Redirect, Link } from 'react-router-dom'
+import axios from 'axios'
+
+import apiUrl from '../../../apiConfig'
+
+const ReviewUpdate = (props) => {
+    const [review, setReview] = useState({ title: '', body: '', rating: '' })
+    const [updated, setUpdated] = useState(false)
+}
+
+useEffect(() => {
+  axios(`${apiUrl}/update-review/${props.match.params.id}`)
+    .then(res => setReview(res.data.review))
+    .catch(console.error)
+}, [])
+
+const handleChange = event => {
+  event.persist()
+  setReview(prevReview => {
+      const updatedField = { [event.target.name]: event.target.value }
+      const editedReview = Object.assign({}, prevReview, updatedField)
+      return editedReview
+  })
+}
+
+const handleSubmit = event => {
+    event.preventDefault()
+    axios({
+      url: `${apiUrl}/reviews/${props.match.params.id}`,
+      method: 'PATCH',
+      data: { review }
+    })
+      .then(() => setUpdated(true))
+      .catch(console.error)
+  }
+
+  if (updated) {
+    return <Redirect to={`/reviews/${props.match.params.id}`} />
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input
+        placeholder="A Wonderful Movie"
+        value={review.title}
+        name="title"
+        onChange={handleChange}
+      />
+      <label>Body</label>
+      <input
+        placeholder="Enter Text Here"
+        value={review.body}
+        name="body"
+        onChange={handleChange}
+      />
+      <label>Rating</label>
+      <input
+        placeholder="Rating"
+        value={review.rating}
+        name="rating"
+        onChange={handleChange}
+      />
+       <button type="submit">Submit</button>
+      <Link to={'update-show/'}>
+        <button>Cancel</button>
+      </Link>
+    </form>
+  )
+
+
+export default ReviewUpdate
+
