247f758d911fc7c95895260785a7455ae0303da6
Katie Veneziano <kmveneziano@comcast.net>
2020-12-03 14:04:56 -0500

    Updated API link to correspond to our deployed backend

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://hidden-river-83146.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
